db74b4e781ca74b80b7c45deb71d767d294dd0f3
abellilo <abel.ayinde@gmail.com>
2025-08-11 15:16:36 +0100

    working on first and second section

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*================== typed js =======================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger',],
+    strings: ['Web Developer', 'Mobile App Developer', 'Artificial Intelligence Developer',],
     typeSpeed: 100, 
     backSpeed: 100, 
     backDelay: 1000, 
